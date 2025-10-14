02fadf05800499b98eeae3684cd9c6b98648b930
Denilson Bulhões <denilson1569@hotmail.com>
2025-10-14 19:53:31 -0300

    add swagger and openai urls to project

--- middle/src/main.py
+++ middle/src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from src.routes import auth, health
 
-app = FastAPI(title="Middle Service - Energy Management")
+app = FastAPI(
+    title="Middle Service - Energy Management",
+    description="Middleware responsável pela autenticação e roteamento entre o front e o back.",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(health.router, tags=["health"])
